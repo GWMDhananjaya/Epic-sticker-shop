74aea8a43da28cb85066d36c3162a53b6bc48d62
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-02-22 17:59:14 +0530

    make header part

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,57 +1,29 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function Header() {
-  const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState('');
-  const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('searchTerm', searchTerm);
-    const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`);
-  };
+ 
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, []);
-  // }, [location.search]);
   return (
     <header className='bg-soft-green shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-        <h1 className="font-bold text-lg sm:text-2xl flex items-center gap-3 relative">
-  <span className="text-slate-600 ">epic</span>
-  <div className="relative flex flex-col items-center">
-    {/* Bouncing dot */}
-  <span className="absolute top-[1px]  w-3 h-3 sm:w-3 sm:h-3 bg-green-500 rounded-full animate-bounce"></span>
-    {/* Green underline */}
-    <span className="block h-0.5 w-19 bg-green-500 "></span>
-  </div>
-</h1>
-
-
-
+          <h1 className="font-bold text-lg sm:text-2xl flex items-center gap-3 relative">
+            <span className="text-slate-600">epic</span>
+            <div className="relative flex flex-col items-center">
+              <span className="absolute top-[1px] w-3 h-3 sm:w-3 sm:h-3 bg-green-500 rounded-full animate-bounce"></span>
+              <span className="block h-0.5 w-20 bg-green-500"></span>
+            </div>
+          </h1>
         </Link>
-        <form
-          onSubmit={handleSubmit}
-          className='bg-slate-100 p-3 rounded-lg flex items-center'
-        >
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <button>
+          <button type='submit'>
             <FaSearch className='text-slate-600' />
           </button>
         </form>
@@ -66,17 +38,7 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/profile'>
-            {currentUser ? (
-              <img
-                className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.avatar}
-                alt='profile'
-              />
-            ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
-            )}
-          </Link>
+        
         </ul>
       </div>
     </header>
