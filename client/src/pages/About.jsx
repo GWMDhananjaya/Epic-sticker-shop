fd124c97f41d357896a82563e9c8af94717ab0a9
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2025-02-24 21:40:41 +0530

    make about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,9 @@
-
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Epic</h1>
+      <p className='mb-4 text-slate-700'>Welcome to Epic, your ultimate destination for high-quality vehicle modification parts and custom stickers. We specialize in offering a wide range of accessories to enhance your ride, from performance upgrades to unique decals. Our mission is to provide top-notch products and excellent customer service, helping you find the perfect fit for your vehicle with ease.</p>
+      <p className='mb-4 text-slate-700'>Take your ride to the next level with Epic !</p>
+     </div>
   )
 }
